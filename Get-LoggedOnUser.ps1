47356485c03e0a174e47f9622d1a4bedf85dbad2
Hugh Smalley <hugh@thehijax.com>
2012-10-05 18:17:11 -0400

    Just some more scripts
    Just a few things I've collected while solving a few problems.

---
+++ Get-LoggedOnUser.ps1
@@ -0,0 +1,5 @@
+﻿function Get-LoggedOnUser {param([string[]]$Computer)
+    $_ = Get-WmiObject Win32_ComputerSystem -Comp $Computer
+    "Host Name: " + $_.Name
+    "User: " + $_.UserName
+}
---
+++ Parse-IniFile.ps1
@@ -0,0 +1,19 @@
+﻿Function Parse-IniFile ($file) {
+  $ini = @{}
+
+ # Create a default section if none exist in the file. Like a java prop file.
+ $section = "NO_SECTION"
+ $ini [$section] = @{}
+
+  switch -regex -file $file {
+    "^\[(.+)\]$" {
+      $section = $matches[1].Trim()
+      $ini[$section] = @{}
+    }
+    "^\s*([^#].+?)\s*=\s*(.*)" {
+      $name,$value = $matches[1..2]
+      $ini[$section][$name] = $value.Trim()
+    }
+  }
+  $ini
+}
