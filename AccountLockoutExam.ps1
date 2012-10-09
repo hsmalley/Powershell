e92a962984b9be840cbbb52d1856c74df175654e
Hugh Smalley <hugh@thehijax.com>
2012-10-09 06:51:23 -0400

    Account Lock Exam Added
    Started refining an acount lockout tool.

---
+++ AccountLockoutExam.ps1
@@ -0,0 +1,7 @@
+﻿#Based off Of
+#http://mikefrobbins.com/2011/10/06/find-ad-user-account-lockout-events-with-powershell/
+$logName = "security"
+$pcName = "DC", "DC2"
+#$eventID = "4740"
+$eventID = "644"
+Get-EventLog -LogName $logName -ComputerName $pcName | Where {$_.eventID -eq $eventID} | Format-List -Property timegenerated, replacementstrings, message
